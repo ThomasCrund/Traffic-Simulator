d074db1b05802be9afeb8decb34a26d6ab93a621
Thomas Crundwell <39323104+ThomasCrund@users.noreply.github.com>
2023-10-05 16:35:55 +1100

    Initial Code

---
+++ car.py
@@ -0,0 +1,26 @@
+from intersection import Light_Colour
+
+class Car:
+  
+  def __init__(self, direction_from: int, start_time: int, light: Light_Colour, time_to_intersection: float):
+    self.dir = direction_from
+    self.start_time = start_time
+    self.weight = 0.0
+    self.initial_light = light
+    self.time_to_intersection = time_to_intersection
+    self.update_weight(light, start_time)
+    self.passed_sensors = 1
+    self.through_intersection = False
+
+  def update_weight(self, light: Light_Colour, current_time: int):
+    if self.initial_light == Light_Colour.GREEN or self.initial_light == Light_Colour.ORANGE:
+      self.weight = 2.0
+    else:
+      self.weight = 1.0
+    
+    if current_time > self.start_time + self.time_to_intersection and light == Light_Colour.RED:
+      self.weight += 0.2 * (current_time - self.start_time - self.time_to_intersection)
+
+    return self.weight
+
+  
---
+++ intersection.py
@@ -0,0 +1,75 @@
+from typing import List
+from enum import Enum
+from car import Car
+
+class Light_Colour(Enum):
+  GREEN = 1
+  ORANGE = 2
+  RED = 3
+
+class Sensor:
+  def __init__(self, num: int, time_to_intersection: int, sensor_order_num = True):
+    self.num = num
+    self.time_to_intersection = time_to_intersection
+    self.past_data = {}
+    self.triggered = False
+    self.sensor_order_num = sensor_order_num
+
+  def update(self, triggered: bool, time_stamp: int):
+    self.past_data[time_stamp] = triggered
+    self.triggered = triggered
+
+class Road:
+  def __init__(self, direction: int, sensors: List[Sensor]):
+    self.direction = direction
+    self.sensors = sensors
+    self.current_cars: List[Car] = []
+    self.past_cars: List[Car] = []
+
+class Light_Phase:
+  def __init__(self, roads: List[Road], min_green = 3, max_green = 1, orange_time = 3, red_time = 3):
+    self.roads = roads
+    self.orange_time = 3
+    self.current_colour = Light_Colour.RED
+    self.time_since_green = 
+    if (len(self.roads) == 0):
+      self.orange_time = 0
+
+  def getWeight(self):
+    total_weight = 0.0
+    for road in self.roads:
+      for car in road.current_cars:
+        total_weight += car.weight
+
+  def update_sensors(self, sensors_data: List[bool], light: Light_Colour, time_stamp: int):
+    for sensor in self.sensors:
+      sensor.update(sensors_data[sensor.num])
+      if (sensor.triggered and sensor.sensor_order_num == 1):
+        car = Car(self.direction, time_stamp, light, sensor.time_to_intersection)
+        self.current_cars.append(car)
+      else:
+        print("### Multiple Sensors Not Yet Implemented  ###")
+        exit(1)
+    
+    for car in self.current_cars:
+      if (light != Light_Colour.RED and time_stamp <= car.start_time + car.time_to_intersection):
+        car.through_intersection = True
+        self.past_cars = car
+        self.current_cars.remove(car)
+
+class Intersection:
+  def __init__(self, roads: List[Road], phases: List[Light_Phase]):
+    self.roads = roads
+    self.lights = {}
+    self.current_phase = Light_Phase([])
+    for road in roads:
+      self.lights[road.direction] = Light_Colour.RED
+    self.phases = phases
+
+  def process_sensor_input(self, sensors_data: List[bool], time_stamp: int):
+    for road in self.roads:
+      road.update_sensors(sensors_data, self.lights[road.direction], time_stamp)
+    
+
+  def change_phase(self):
+    pass
